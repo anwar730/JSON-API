72b5ebf388c7e30979c4cd61c4d55ac98ffabaa5
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-10-07 21:59:23 +0300

    added db.json

---
+++ db.js
@@ -0,0 +1,9 @@
+const rings = require("./data/rings");
+const watches = require("./data/watches");
+const necklaces = require("./data/necklaces")
+
+module.exports = {
+    rings,
+    watches,
+    necklaces
+}
